a8e584f1bda9e58d1f80d1ed72c92d88fccecafd
GitStudentSem <programmSem@yandex.ru>
2021-08-09 17:37:39 +0300

    create Music, Settings, News, Dialog components set up switching by links

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from "react";
 import "./App.css";
 import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+// Импорт компонент из react-router-dom не забыть сделать
+// необходимо установить пакет react-router-dom
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-        <Profile />
-        {/* <Dialogs /> */}
+    // BrowserRouter необходимая обертка для работы Router,
+    //необходимо установить пакет react-router-dom и импортировать его
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          {/* Роут для переключения между ссылками необходимо установить пакет react-router-dom
+        и импортировать его */}
+          <Route path="/dialogs" component={Dialogs} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/news" component={News} />
+          <Route path="/music" component={Music} />
+          <Route path="/settings" component={Settings} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,27 +4,27 @@ const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={`${s.item} ${s.active}`}>
-        <a className={s.link} href="#d">
+        <a className={s.link} href="/profile">
           Profile
         </a>
       </div>
       <div className={s.item}>
-        <a className={s.link} href="#d">
+        <a className={s.link} href="/dialogs">
           Messages
         </a>
       </div>
       <div className={s.item}>
-        <a className={s.link} href="#d">
+        <a className={s.link} href="/news">
           News
         </a>
       </div>
       <div className={s.item}>
-        <a className={s.link} href="#d">
+        <a className={s.link} href="/music">
           Music
         </a>
       </div>
       <div className={s.item}>
-        <a className={s.link} href="#d">
+        <a className={s.link} href="/settings">
           Settings
         </a>
       </div>
