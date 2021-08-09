95c2395a1bb4ced2ed1cd04be22c19f67591d1fb
GitStudentSem <programmSem@yandex.ru>
2021-08-09 14:24:42 +0300

    add comment & like props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -9,11 +9,18 @@ const MyPosts = () => {
         <button>Post</button>
         <div>New post</div>
         <div className={s.posts}>
-          <Post message="Первый нах" />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
+          <Post message="Первый нах" likeCount="12" />
+          <Post message="Второй нах" likeCount="52" />
+          <Post message="первоход" likeCount="1111" />
+          <Post
+            message="Далеко-далеко, за словесными горами в стране гласных и согласных живут рыбные тексты"
+            likeCount=""
+          />
+          <Post
+            message="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut officiis
+        ipsam nisi obcaecati veritatis rem recusandae!"
+            likeCount="0"
+          />
         </div>
       </div>
     </div>
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import s from "./Post.module.css";
 import userAvatar from "./user.svg";
-const Post = () => {
+const Post = (props) => {
   return (
     <div className={s.item}>
       <img className={s.avatar} src={userAvatar} alt={"user avatar"} />
-      <p className={s.text}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut officiis
-        ipsam nisi obcaecati veritatis rem recusandae! Adipisci earum laboriosam
-        consequatur at nobis ut pariatur vero libero, temporibus, reprehenderit
-        officiis tenetur!
-      </p>
-      <button className={s.like}></button>
+      <p className={s.text}>{props.message}</p>
+      <div className={s.like}>
+        <button className={s.like_button}></button>
+        <p className={s.like_count}>{props.likeCount}</p>
+      </div>
     </div>
   );
 };
