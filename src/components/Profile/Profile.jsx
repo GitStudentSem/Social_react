34fb2a64f5d129ddfc4b5d9f93b074e02e773e62
GitStudentSem <programmSem@yandex.ru>
2021-08-09 16:59:12 +0300

    create profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import s from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className={s.content}>
+    <div>
       <div className={s.image}></div>
       <div>avatar + description</div>
       <MyPosts />
