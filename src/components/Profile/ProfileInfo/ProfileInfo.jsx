9d13ca258cf21c1280d05d6fe1f652f0dc3d0235
GitStudentSem <programmSem@yandex.ru>
2021-08-10 11:49:09 +0300

    fix user name in message

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import s from "./ProfileInfo.module.css";
+import avatar from "./user.svg";
 
 const ProfileInfo = () => {
   return (
     <div>
       <div className={s.image}></div>
-      <div>avatar + description</div>
+      <div>
+        <img src={avatar} alt={"user avatar"} className={s.avatar} />
+        <p className={s.name}>Имя Фамилия</p>
+      </div>
     </div>
   );
 };
