25e5d51286786cac5bb3aa156ec1f1a6313f6cb9
GitStudentSem <programmSem@yandex.ru>
2021-08-10 09:01:57 +0300

    Configured toggling dialogs. Customized the appearance of dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from "./Dialogs.module.css";
 import avatar from "./user.svg";
 
@@ -6,22 +7,38 @@ const Dialogs = (props) => {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogs_item}>
-        <div className={`${s.dialog} ${s.active}`}>
+        <NavLink
+          className={s.dialog}
+          to="/dialogs/1"
+          activeClassName={s.active}
+        >
           <img src={avatar} alt={"user avatar"} className={s.avatar} />
           <p className={s.name}>Первый пользователь</p>
-        </div>
-        <div className={s.dialog}>
+        </NavLink>
+        <NavLink
+          className={s.dialog}
+          to="/dialogs/2"
+          activeClassName={s.active}
+        >
           <img src={avatar} alt={"user avatar"} className={s.avatar} />
           <p className={s.name}>Второй пользователь</p>
-        </div>
-        <div className={s.dialog}>
+        </NavLink>
+        <NavLink
+          className={s.dialog}
+          to="/dialogs/3"
+          activeClassName={s.active}
+        >
           <img src={avatar} alt={"user avatar"} className={s.avatar} />
           <p className={s.name}>Третий пользователь</p>
-        </div>
-        <div className={s.dialog}>
+        </NavLink>
+        <NavLink
+          className={s.dialog}
+          to="/dialogs/4"
+          activeClassName={s.active}
+        >
           <img src={avatar} alt={"user avatar"} className={s.avatar} />
           <p className={s.name}>Четвертый пользователь</p>
-        </div>
+        </NavLink>
       </div>
       <div className={s.line}></div>
       <div className={s.messages}>
