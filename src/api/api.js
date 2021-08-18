869b2d7245afd3c98e4321351f77f53bc87e12fe
GitStudentSem <programmSem@yandex.ru>
2021-08-18 20:25:47 +0300

    Moved the functions for working with the server into a separate api.js file

---
+++ src/api/api.js
@@ -0,0 +1,17 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  headers: {
+    "API-KEY": "4005e396-5edb-48e9-82c7-a6111fe54f90",
+  },
+});
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance
+      .get(`users?page=${currentPage}&count=${pageSize}`)
+      .then((response) => response.data);
+  },
+};
