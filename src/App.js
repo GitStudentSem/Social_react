347267b2254170ce70121c67c6f73311e2f43a14
GitStudentSem <programmSem@yandex.ru>
2021-08-18 14:57:40 +0300

    Added withRouter to track user id in URL

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = (props) => {
             и импортировать его */}
           {/* Route принимает 2 метода:  render и component через render можно передать props*/}
           <Route path="/dialogs" render={() => <DialogsContainer />} />
-          <Route path="/profile" render={() => <ProfileContainer />} />
+          <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
           <Route path="/users" render={() => <UsersContainer />} />
           <Route path="/news" render={() => <News />} />
           <Route path="/music" render={() => <Music />} />
