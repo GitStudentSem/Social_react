e5e7ed0b5262879789b92c4610594ff365a8d909
GitStudentSem <programmSem@yandex.ru>
2021-09-19 13:23:44 +0300

    Set up validation for the login form

--- src/components/common/FormsControls/FormsControls.jsx
+++ src/components/common/FormsControls/FormsControls.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import s from "./FormsControls.module.css";
-export const Input = ({ input, meta, ...props }) => {
+
+const FormControl = ({ input, meta, child, ...props }) => {
   const hasError = meta.touched && meta.error;
+
   return (
     <div className={s.formControl + " " + (hasError ? s.error : "")}>
-      <div>
-        <input {...input} {...props} />
-      </div>
-
+      <div>{props.children}</div>
       {hasError && <span>{meta.error}</span>}
     </div>
+  );
+};
+
+export const Input = (props) => {
+  const { input, meta, child, ...restProps } = props;
+  return (
+    <FormControl {...props}>
+      <input {...input} {...restProps} />
+    </FormControl>
   );
 };
